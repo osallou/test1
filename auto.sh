31edb76f836c0a1a437a19e21cc21729df3f2d12
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-12 14:20:55 +0200

    fix auto

--- auto.sh
+++ auto.sh
@@ -11,15 +11,21 @@ do
     if [ $curfile == "Dockerfile" ]
     then
         echo "PLUGIN_DOCKERFILE=$p" > DRONE_ENV
-        major=`sed -n 's/LABEL\s*software.version="\(.*\)"/\1/p' $p`
-        minor=`sed -n 's/LABEL\s*version="\(.*\)"/\1/p' $p`
-        version=$major-$minor
+        major=`sed -n 's/.*\ssoftware.version="\(.*\)"/\1/p' $p`
+        minor=`sed -n 's/.*\sversion="\(.*\)"/\1/p' $p`
+        version=$major-minor
+        if [ $minor == "" ]
+        then
+            version=$major
+        fi
+        software=`sed -n 's/.*\ssoftware="\(.*\)"/\1/p' $p`
         if [ $version == "-" ]
         then
             echo "Could not extract version from Dockerfile: $p"
             exit 1
         fi
         echo "PLUGIN_TAG=$version" >> DRONE_ENV
+        echo "PLUGIN_REPO=osallou/$software" >> DRONE_ENV
     fi
 
 done < /tmp/out
