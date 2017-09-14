c4c47ee85977f9d3df8c1495d7aa1056027159d9
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-14 12:11:03 +0200

    test privileged cont

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/local/bin/dockerd --data-root /var/lib/docker
+/usr/local/bin/dockerd --data-root /var/lib/docker &
 sleep 10
 . ./DRONE_ENV
 /usr/local/bin/docker run --rm $1 echo hello $PLUGIN_TAG
