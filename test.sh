646eca1b276fb17c1a1b2e60d63696f33bf9d436
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-20 14:16:41 +0200

    add registry

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ if [ -e ./$BIOCONTAINER_DIR/test.sh ]; then
     /usr/local/bin/dockerd --data-root /var/lib/docker --dns 192.168.100.43&
     echo "Wait for docker daemon"
     sleep 10
-    echo "Creds:  $${DOCKER_USERNAME} -p $${DOCKER_PASSWORD}"
+    echo "Creds:  "$${DOCKER_USERNAME} $${DOCKER_PASSWORD}
     /usr/local/bin/docker login -u $${DOCKER_USERNAME} -p $${DOCKER_PASSWORD} $PLUGIN_REGISTRY
     /usr/local/bin/docker run -v ${PWD}/$BIOCONTAINER_DIR:/mnt/biocontainers --rm $PLUGIN_REPO:$PLUGIN_TAG /mnt/biocontainers/test.sh
 else
