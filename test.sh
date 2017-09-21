0ed9de4302b25f13d97a3ed35fb066561bed7aac
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-21 10:46:26 +0200

    test network issues

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ if [ -e ./$BIOCONTAINER_DIR/test.sh ]; then
     /usr/local/bin/dockerd --data-root /var/lib/docker --dns 192.168.100.43&
     echo "Wait for docker daemon"
     sleep 10
+    ip a
+    ping openstack-192-168-100-43.genouest.org
     /usr/local/bin/docker run -v ${PWD}/$BIOCONTAINER_DIR:/mnt/biocontainers --rm $PLUGIN_REPO:$PLUGIN_TAG /mnt/biocontainers/test.sh
 else
     echo "No test script available"
