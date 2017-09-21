80e89c6bb1b663e9aee1eb0795b158ce16450322
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-21 12:58:02 +0200

    test autotest

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ set -e
 . ./DRONE_ENV
 echo "check for ./$BIOCONTAINER_DIR/test.sh"
 if [ -e ./$BIOCONTAINER_DIR/test.sh ]; then
+    echo "Launch docker daemon"
     /usr/local/bin/dockerd --data-root /var/lib/docker --dns 192.168.100.43&
     echo "Wait for docker daemon"
     /usr/local/bin/docker run -v ${PWD}/$BIOCONTAINER_DIR:/mnt/biocontainers --rm $PLUGIN_REPO:$PLUGIN_TAG /mnt/biocontainers/test.sh
