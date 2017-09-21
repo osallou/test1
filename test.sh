28d0caad4c8e4058f6977537c6aabacb3c17c72a
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-21 10:52:06 +0200

    test network issues

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ if [ -e ./$BIOCONTAINER_DIR/test.sh ]; then
     /usr/local/bin/dockerd --data-root /var/lib/docker --dns 192.168.100.43&
     echo "Wait for docker daemon"
     sleep 10
-    echo "user: " ${DOCKER_USERNAME}
-    docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} openstack-192-168-100-43.genouest.org
+    echo "user: " ${TEST_USERNAME}
+    docker login -u ${TEST_USERNAME} -p ${TEST_PASSWORD} openstack-192-168-100-43.genouest.org
     /usr/local/bin/docker run -v ${PWD}/$BIOCONTAINER_DIR:/mnt/biocontainers --rm $PLUGIN_REPO:$PLUGIN_TAG /mnt/biocontainers/test.sh
 else
     echo "No test script available"
