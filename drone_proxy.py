fb951d2586714bb8a7f537c42d0d71233f6ee7ac
Olivier Sallou <olivier.sallou@irisa.fr>
2017-10-11 09:18:29 +0200

    update scripts

--- drone_proxy.py
+++ drone_proxy.py
@@ -3,12 +3,16 @@ from flask import request
 import json
 import copy
 import logging
-
+import os
 
 import arrow
 import requests
 
 app = Flask(__name__)
+
+drone_url = 'http://localhost:8000'
+if 'DRONE_URL' in os.environ:
+    drone_url = os.environ['DRONE_URL']
 
 @app.route('/hook', methods=['POST'])
 def payload():
@@ -17,17 +21,22 @@ def payload():
         try:
             payload = json.loads(request.form.get('payload'))
             commits = payload['commits']
+            drone_modified = False
             new_commits = {}
             for commit in commits:
                 modified_container = []
                 modified_commits = []
                 updated = False
                 for modified in commit['added']:
+                    if modified.startswith('.drone.yml'):
+                        drone_modified = True
                     container_path = '/'.join(modified.split('/')[:-1])
                     if container_path and container_path not in modified_container:
                         modified_container.append(container_path + '/Dockerfile')
                         updated = True
                 for modified in commit['modified']:
+                    if modified.startswith('.drone.yml'):
+                        drone_modified = True
                     container_path = '/'.join(modified.split('/')[:-1])
                     if container_path and container_path not in modified_container:
                         modified_container.append(container_path + '/Dockerfile')
@@ -51,12 +60,16 @@ def payload():
                     else:
                         if new_commits[container_dir]['date'] > arrow.get(commit['timestamp']).datetime:
                             new_commits[container_dir] = {'date': arrow.get(commit['timestamp']).datetime, 'payload': commit_dict}
+            event = request.headers['X-GitHub-Event']
+            if drone_modified and event == 'pull_request':
+                logging.error('tried to modified .drone.yml in a pull request, forbidden:' + json.dumps(payload))
+                return "ko"
             for d in new_commits:
-                logging.warn('headers: '+str(request.headers))
-                logging.warn('data: '+json.dumps(new_commits[d]['payload']))
-                r = requests.post('http://localhost:8000/hook?access_token='+request.form.get('access_token'), data={'payload': json.dumps(new_commits[d]['payload'])}, headers=request.headers)
-                logging.warn('post result '+str(r.status_code))
-                logging.warn(str(r.text))
+                logging.debug('headers: '+str(request.headers))
+                logging.debug('data: '+json.dumps(new_commits[d]['payload']))
+                r = requests.post(drone_url + '/hook?access_token='+request.form.get('access_token'), data={'payload': json.dumps(new_commits[d]['payload'])}, headers=request.headers)
+                logging.debug('post result '+str(r.status_code))
+                logging.debug(str(r.text))
                 # print(json.dumps(new_commits[d]['payload']))
         except Exception as e:
             logging.exception(str(e))
@@ -65,4 +78,3 @@ def payload():
 
 if __name__ == '__main__':
           app.run(host='0.0.0.0')
-
