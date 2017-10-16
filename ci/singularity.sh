62b4d38953d19f7c52981109974c7f33e9e52b5c
Olivier Sallou <olivier.sallou@irisa.fr>
2017-10-16 17:31:37 +0200

    dont rebuild

--- ci/singularity.sh
+++ ci/singularity.sh
@@ -2,17 +2,17 @@
 
 . ./DRONE_ENV
 
-apt-get update && \
-    apt-get install -y \
-    python \
-    dh-autoreconf \
-    build-essential \
-    squashfs-tools \
-    git
+#apt-get update && \
+#    apt-get install -y \
+#    python \
+#    dh-autoreconf \
+#    build-essential \
+#    squashfs-tools \
+#    git
 
 
-git clone https://github.com/singularityware/singularity.git
-cd singularity && ./autogen.sh && ./configure --prefix=/usr/local --sysconfdir=/etc && make && make install
+#git clone https://github.com/singularityware/singularity.git
+#cd singularity && ./autogen.sh && ./configure --prefix=/usr/local --sysconfdir=/etc && make && make install
 
 echo "Bootstrap: docker" > Singularity
 echo "From: $PLUGIN_REPO:$PLUGIN_TAG" >> Singularity
@@ -20,6 +20,6 @@ echo "From: $PLUGIN_REPO:$PLUGIN_TAG" >> Singularity
 cat Singularity
 
 mkdir -p ci/img
-export SINGULARITY_MESSAGELEVEL=-3
-export PYTHONIOENCODING=UTF-8
+#export SINGULARITY_MESSAGELEVEL=-3
+#export PYTHONIOENCODING=UTF-8
 /usr/local/bin/singularity build ci/img/$SOFTWARE_NAME_$PLUGIN_TAG.img Singularity
