d89eb738c2004e94c0cdd017aa47b3664595b9da
Olivier Sallou <olivier.sallou@irisa.fr>
2017-10-16 13:58:43 +0200

    fixes

--- ci/proxy/drone_proxy.py
+++ ci/proxy/drone_proxy.py
@@ -14,36 +14,55 @@ drone_url = 'http://localhost:8000'
 if 'DRONE_URL' in os.environ:
     drone_url = os.environ['DRONE_URL']
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "pong"
-
 @app.route('/hook', methods=['POST'])
 def payload():
         # print(request.content_type)
         # print(str(request.form))
+        logging.error(request.form)
         try:
             payload = json.loads(request.form.get('payload'))
-            commits = payload['commits']
+            commits = []
+            if 'commits' in payload:
+                commits = payload['commits']
+            elif 'pull_request' in payload:
+                res = requests.get(
+                    payload['pull_request']['url'] + '/files',
+                    headers={'Accept': 'application/vnd.github.v3+json'}
+                )
+                files = res.json()
+                containers = []
+                for pull_file in files:
+                    if pull_file['filename'].startswith('.drone.yml') or pull_file['filename'].startswith('ci/'):
+                        logging.error('tried to modified .drone.yml or ci scripts in a pull request, forbidden:' + json.dumps(payload))
+                        return "ko, can't modify .drone.yml or ci/ files"
+                    container_path = '/'.join(pull_file['filename'].split('/')[:-1])
+                    if container_path not in containers:
+                        containers.append(container_path)
+                if len(containers) > 1:
+                    logging.error("ko, can't modify multiple containers in a same pull request")
+                    return "ko, can't modify multiple containers in a same pull request"
+                r = requests.post(drone_url + '/hook?access_token='+request.form.get('access_token'), data={'payload': json.dumps(payload)}, headers=request.headers)
+                return "ok"
+
             drone_modified = False
             new_commits = {}
             for commit in commits:
                 modified_container = []
                 modified_commits = []
                 updated = False
+                if 'added' not in commit:
+                    commit['added'] = []
                 for modified in commit['added']:
-                    if modified.startswith('ci'):
-                        continue
-                    if modified.startswith('.drone.yml'):
+                    if modified.startswith('.drone.yml') or modified.startswith('ci/'):
                         drone_modified = True
                     container_path = '/'.join(modified.split('/')[:-1])
                     if container_path and container_path not in modified_container:
                         modified_container.append(container_path + '/Dockerfile')
                         updated = True
+                if 'modified' not in commit:
+                    commit['modified'] = []
                 for modified in commit['modified']:
-                    if modified.startswith('ci'):
-                        continue
-                    if modified.startswith('.drone.yml'):
+                    if modified.startswith('.drone.yml') or modified.startswith('ci/'):
                         drone_modified = True
                     container_path = '/'.join(modified.split('/')[:-1])
                     if container_path and container_path not in modified_container:
@@ -55,8 +74,8 @@ def payload():
                     commit['added'] = []
                     commit['modified'] = [modif]
                     commit['removed'] = []
-                    container_dir = '/'.join(modif.split('/')[:-1])
                     commit['message'] = container_dir + ':' + commit['message']
+                    container_dir = '/'.join(modif.split('/')[:-1])
                     commit_dict = copy.deepcopy(payload)
                     commit_dict['commits'] = [commit]
                     commit_dict['head_commit'] = commit
@@ -70,7 +89,7 @@ def payload():
                             new_commits[container_dir] = {'date': arrow.get(commit['timestamp']).datetime, 'payload': commit_dict}
             event = request.headers['X-GitHub-Event']
             if drone_modified and event == 'pull_request':
-                logging.error('tried to modified .drone.yml in a pull request, forbidden:' + json.dumps(payload))
+                logging.error('tried to modified .drone.yml or ci scripts in a pull request, forbidden:' + json.dumps(payload))
                 return "ko"
             for d in new_commits:
                 logging.debug('headers: '+str(request.headers))
